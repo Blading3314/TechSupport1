613e092eea3939d995078c0e53e4cfbc7c8c4e4c
Thomas <thomascastanop@gmail.com>
2025-03-06 09:38:37 -0500

    new class question 14

---
+++ RandomTester.java
@@ -0,0 +1,37 @@
+
+/**
+ * Write a description of class RandomTester here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class RandomTester
+{
+    // instance variables - replace the example below with your own
+    private int number;
+
+    /**
+     * Constructor for objects of class RandomTester
+     */
+    public RandomTester()
+    {
+        // initialise instance variables
+        number = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int printOneRandom(int y)
+    {
+        // put your code here
+       
+    }
+    public int printMultiRando(int howMany){
+        
+    }
+    
+}
