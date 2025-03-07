2afae8e07c687781a005ab31416d8e7c563cad46
Le Tom <161770957+Blading3314@users.noreply.github.com>
2025-03-07 11:16:24 -0500

    Add files via upload

--- Responder.java
+++ Responder.java
@@ -10,7 +10,7 @@ public class Responder
 {
     private Random random_generator;
     private ArrayList<String> responses;
-    
+   
     /**
      * Construct a Responder - nothing to do
      */
@@ -18,17 +18,22 @@ public class Responder
     {
      random_generator = new Random();
      responses = new ArrayList<>(); 
-     fillResponses(); 
+    
+     fillResponses();
+
     }
     /**
      * Generate a response.
      * @return   A string that should be displayed as the response
-     */
+     */ //27
     public String generateResponse()
     {
         int index = random_generator.nextInt(responses.size()); 
         return responses.get(index);
     
+    }
+    private String pickDefaultResponse(){
+        return "no idea, sorry"; 
     }
     public void fillResponses(){ //22
         responses.add("yes");
@@ -38,4 +43,11 @@ public class Responder
         responses.add("Maybe");
         responses.add("No idea");
     }
+    public String generateResponse(String word){ 
+        String responses = responses.get(word);
+        if (answer == null){ 
+            response = pickDefaultResponse(); 
+}
+return response;
+}
 }
