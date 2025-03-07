d4cc236da974adf61caa6a0791d89fe3d0bee0d3
Le Tom <161770957+Blading3314@users.noreply.github.com>
2025-03-07 09:03:11 -0500

    Add files via upload

--- RandomTester.java
+++ RandomTester.java
@@ -1,10 +1,10 @@
-import class java.util.class.Random
+import java.util.Random;
 /**
  * Write a description of class RandomTester here.
  *
  * @author (your name)
  * @version (a version number or a date)
- */
+ */ //question 14 below
 public class RandomTester
 {
     // instance variables - replace the example below with your own
@@ -25,13 +25,27 @@ public class RandomTester
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public int printOneRandom(int y)
+    public void printOneRandom()
     {
-        System.
+        System.out.println("Random number: "+ random_generator.nextInt(1000)); //15
        
     }
-    public int printMultiRando(int howMany){
+    public void printMultiRandom(int howMany){
+        int index = 0;
+        while(index < howMany){
+            printOneRandom(); 
+            index++; 
+        }
         
     }
-    
+    public String getResponse(){ //18
+        int index = random_generator.nextInt(3);
+        if (index ==0){
+            return "yes"; 
+        } else if(index == 1){
+            return "no";
+        } else{
+            return "maybe"; 
+        }
+    }
 }
